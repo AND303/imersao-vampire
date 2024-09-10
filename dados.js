904ed9de0562848a509cd32fec7779117fe9d797
AND303 <106715104+AND303@users.noreply.github.com>
2024-09-10 00:46:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,51 @@
+let dados = [
+    {
+        titulo : "Ventrue",
+        imagem : "./img/Ventrue_symbol.png",
+        alt : "Imagem do clã Ventrue",
+        descricao : "Os Ventrue são frequentemente referidos como o Clã dos Reis. Com uma longa história de liderança e influência, os membros desse clã são conhecidos por sua ambição, diplomacia e senso de direito. Oriundos da nobreza e da aristocracia, os Ventrue valorizam a tradição, o poder e o status social. Seus domínios se estendem por diversas esferas da sociedade, desde a política e os negócios até a cultura e a arte. A disciplina e a autocontrole são marcas registradas dos Ventrue, que buscam moldar o mundo a sua imagem, impondo sua visão de ordem e progresso.",
+        disciplina : "Dominação, Fortitude, Presença",
+        tags: "camarilla Ventru"
+    },
+    {
+        titulo : "Toreador",
+        imagem : "./img/Toreador_symbol.png",
+        alt : "Imagem do clã Toreador",
+        descricao : "Os Toreador são conhecidos por sua paixão pelas artes, beleza e sensualidade. Valorizam a beleza em todas as suas formas e são frequentemente encontrados em salões de arte, teatros e outros ambientes culturais. Sua busca pela perfeição estética os leva a criar obras de arte e performances que transcendem a mortalidade. No entanto, essa obsessão pela beleza pode levar a um narcisismo extremo e a uma visão elitista da sociedade.",
+        disciplina : "Auspícios, Celeridade, Presença",
+        tags: "camarilla"
+    },
+    {
+        titulo : "Malkavianos",
+        imagem : "./img/1024px-Malkavian_symbol.png",
+        alt : "Imagem do clã Malkaviano",
+        descricao : "Os Malkavianos são conhecidos por sua loucura e visão distorcida da realidade. Suas mentes são fragmentadas e eles frequentemente têm visões proféticas ou precognitivas. Apesar de sua instabilidade, muitos Malkavian possuem grande inteligência e podem ser fontes de informações valiosas. No entanto, sua condição os isola da sociedade e os torna alvos de preconceito e medo.",
+        disciplina : "Auspícios, Dominação, Ofuscação",
+        tags: "camarilla Malcavianos Malkavian"
+    },
+    {
+        titulo : "Tremere",
+        imagem : "./img/Tremere_symbol.png",
+        alt : "Imagem do Tremere",
+        descricao : "Os Tremere são os magos entre os vampiros, mestres da magia negra e da manipulação. Com uma longa história de intrigas e ambições, os Tremere buscam o conhecimento e o poder a qualquer custo. Seus domínios se estendem por diversas áreas da magia, desde a necromancia até a alquimia. A disciplina e a inteligência são marcas registradas dos Tremere, que buscam dominar o mundo através do conhecimento.",
+        disciplina : "Auspícios, Feitiçaria de Sangue, Dominação",
+        tags: "camarilla"
+    },
+    {
+        titulo : "Nosferatu",
+        imagem : "./img/Nosferatu_symbol.png",
+        alt : "Imagem do clã Nosferatu",
+        descricao : " Os Nosferatu são os párias da sociedade vampiresca, amaldiçoados com uma aparência monstruosa. Exilados nas sombras, os Nosferatu são mestres da espionagem e da infiltração. Seus domínios se estendem por diversas áreas da criminalidade e da subcultura. A astúcia e a resiliência são marcas registradas dos Nosferatu, que sobrevivem nas profundezas da sociedade.",
+        disciplina : "Animalismo, Ofuscação, Potência",
+        tags: "camarilla Nosferatus"
+    },
+    {
+        titulo : "Banu Haqim",
+        imagem : "./img/Banu_Haqim_Symbol.png",
+        alt : "Imagem do clã Banu Haqim",
+        descricao : " Os juízes dos Banu Haqim estão divididos entre sua sede hereditária por Sangue vampírico e sua paixão pela justiça. Árbitros severos, eles são ferozmente devotados a manter um código moral e Abraçar mortais capazes de avaliar e lidar com ameaças, fazer cumprir as leis e tradições e punir transgressores.",
+        disciplina : "Feitiçaria do Sangue, Celeridade, Ofuscação",
+        tags: "camarilla"
+    },
+];
+
